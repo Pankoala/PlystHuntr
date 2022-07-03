bd4a8ed61ab448b9f014579788ae24df2e2b9bd2
Marina <marina@pankoala.space>
2022-07-03 00:09:23 -0500

    Se sube actualización con contenido semidinámico. Las cards en el home se generan a partir de las playlists dadas de alta en las bases de datos

--- ph_app/urls.py
+++ ph_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('catalogo/', views.catalogo, name="catalogo"),
     path("login/", views.login_usuario, name="login"),
+    path("playlist/", views.playlist, name="playlist"),
+    path("subirpl/", views.playlist, name="subirpl"),
 ]
--- ph_app/views.py
+++ ph_app/views.py
@@ -3,19 +3,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from requests_html import HTMLSession
 import requests
 
 from .models import Playlister, Playlist
 
 # Create your views here.
 def index(request):
-   """ Vista para atender la petición de la url / """
-   return render(request, "ph_app/index.html")
-
-
-def catalogo(request):
-   """ Vista o función que atiende la url GET /catalogo/ """
-   playlist = Playlist.objects.get(nombre = "Random")
+   """ Vista o función que atiende la url GET /playlist/"""
+   playlist = Playlist.objects.get(nombre = "Saturn Waves")
    url_image = ""
    if playlist.plataforma == "SP":
       html = requests.get(playlist.link).text
@@ -24,13 +20,76 @@ def catalogo(request):
       url_image = meta.attrs["content"]
       id_spotify = playlist.link.split("?")[0].split("/")[-1]
       url_playlist = f"https://open.spotify.com/embed/playlist/{id_spotify}"
+
    elif playlist.plataforma == "DZ":
-      url_playlist = ""
+      html = requests.get(playlist.link).text
+      soup = BeautifulSoup(html, 'html.parser')
+      meta = soup.find(property = "og:image")
+      url_image = meta.attrs["content"]
+      og_deezer = soup.find(property = "og:url")
+      url_deezer = og_deezer.attrs["content"]
+      id_deezer = url_deezer.split("/")[-1]
+      url_playlist = f"https://widget.deezer.com/widget/dark/playlist/{id_deezer}"
+
    elif playlist.plataforma == "TD":
-      url_playlist = ""
-   return render(request, "ph_app/catalogo.html", {"url_playlist": url_playlist, "playlist": playlist, "url_image" : url_image})
+      # session = HTMLSession()
+      # html = session.get(playlist.link).text
+      # soup = BeautifulSoup(html, 'html.parser')
+      # meta = soup.find(property == "og:image")
+      # url_image = meta.attrs["content"]
+      id_tidal = playlist.link.split("/")[-1]
+      url_playlist = f"https://embed.tidal.com/playlists/{id_tidal}?layout=gridify"
+
+   playlists_all = Playlist.objects.all()
+      
+   return render(request, "ph_app/index.html", {"url_playlist": url_playlist, "playlist": playlist, "url_image" : url_image, "playlists": playlists_all,})
 
 
+def catalogo(request):
+   """ Vista para atender la petición de la url /catálogo/ """
+   return render(request, "ph_app/catalogo.html")
+
+
+def subirpl(request):
+   """ Vista para atender la petición de la url /subirpl/ """
+   return render(request, "ph_app/subirpl.html")
+
+
+def playlist(request):
+   """ Vista o función que atiende la url GET /playlist/"""
+   playlist = Playlist.objects.get(nombre = "Tik Toks Latinos")
+   url_image = ""
+   if playlist.plataforma == "SP":
+      html = requests.get(playlist.link).text
+      soup = BeautifulSoup(html, 'html.parser')
+      meta = soup.find(property = "og:image")
+      url_image = meta.attrs["content"]
+      id_spotify = playlist.link.split("?")[0].split("/")[-1]
+      url_playlist = f"https://open.spotify.com/embed/playlist/{id_spotify}"
+
+   elif playlist.plataforma == "DZ":
+      html = requests.get(playlist.link).text
+      soup = BeautifulSoup(html, 'html.parser')
+      meta = soup.find(property = "og:image")
+      url_image = meta.attrs["content"]
+      og_deezer = soup.find(property = "og:url")
+      url_deezer = og_deezer.attrs["content"]
+      id_deezer = url_deezer.split("/")[-1]
+      url_playlist = f"https://widget.deezer.com/widget/dark/playlist/{id_deezer}"
+
+   elif playlist.plataforma == "TD":
+      # session = HTMLSession()
+      # html = session.get(playlist.link).text
+      # soup = BeautifulSoup(html, 'html.parser')
+      # meta = soup.find(property == "og:image")
+      # url_image = meta.attrs["content"]
+      id_spotify = playlist.link.split("/")[-1]
+      url_playlist = f"https://embed.tidal.com/playlists/{id_spotify}?layout=gridify"
+
+   playlists_all = Playlist.objects.all()
+   playlister = Playlister.objects.all()
+      
+   return render(request, "ph_app/playlist.html", {"url_playlist": url_playlist, "playlist": playlist, "url_image" : url_image, "playlists": playlists_all, "playlister" : playlister})
 
 # def login(request):
 #     """ Vista o función que atiende la url GET /login/ """
